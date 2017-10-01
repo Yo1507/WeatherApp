b687aac3da3ba27f470b704a62a6a1e39a97b6d0
Yo <yoan.denis@live.fr>
2017-10-01 15:51:19 +0200

    install module and param app

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 const request = require('request');
+const _ = require('lodash');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for.',
+            string: true,
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+console.log(argv.a);
+const encodedAddress = encodeURIComponent(argv.a);
 
 request({
-    url: "https://maps.googleapis.com/maps/api/geocode/json?address=18%20rue%20de%20la%20megisserie%2081300%20graulhet",
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    if (error) {
+        console.log('Unable to connect to Google servers.');
+    } else if (body.status === 'OK') {
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    } else if (body.status === 'ZERO_RESULT') {
+        console.log('Unable to find that address.');
+    } else {
+        console.log('Unknown response.');
+    }
 });
